760e117b4b57beec064dfac3dff5a32bc1d874c7
i-n-s-o-m <s.voetee@t-online.de>
2025-07-05 16:04:10 -0400

    refactor comments

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,20 @@
 import requests  # Import the requests library to handle HTTP requests
 
-def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion predict service
-    myobject = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analysed
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
+# Define a function named emotion_detector that takes a string input (text_to_analyse)
+def emotion_detector(text_to_analyse):
+    
+    # URL of the emotion predict service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    # Create a dictionary with the text to be analysed
+    myobject = { "raw_document": { "text": text_to_analyse } }
+    
+    # Set the headers required for the API request
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-    response = requests.post(url, json = myobject, headers=header)  # Send a POST request to the API with the text and header
+    # Send a POST request to the API with the text and header
+    response = requests.post(url, json = myobject, headers=header)
 
-    return response.text  # Return the response text from the API
+    # Return the response text from the API
+    return response.text_to_analyse
+    
