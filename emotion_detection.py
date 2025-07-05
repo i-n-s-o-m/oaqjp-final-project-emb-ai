1479e40d04ebeb427ca29e0601ca86f9c4a866ad
i-n-s-o-m <s.voetee@t-online.de>
2025-07-05 15:51:45 -0400

    fix typo in myobject variable

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,10 +2,9 @@ import requests  # Import the requests library to handle HTTP requests
 
 def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion predict service
-    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analysed
+    myobject = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analysed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
 
     response = requests.post(url, json = myobject, headers=header)  # Send a POST request to the API with the text and header
 
     return response.text  # Return the response text from the API
-    
